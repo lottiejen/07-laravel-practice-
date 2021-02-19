f961243fb572695a38ee77c7754877bd75c4f3d6
Lottie <lottiej17@gmail.com>
2021-02-19 17:37:51 +0000

    functions added w notes

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -10,8 +10,38 @@ class OwnerController extends Controller
 {
     public function index()
     { 
-        $ownersFromDB = Owners::all();
-        return view('welcome', ['owners' => $articlesFromDb]);
+        $ownersFromDb = Owner::all();
+        return view('welcome', ['banannas' => $ownersFromDb]);
+    }
+
+
+    public function show(Owner $owner)
+    {
+        return view ('owners/show', ['owner' => $owner]);
+    }
+
+    public function create()
+    {
+        return view("owners/form"); // this loads the form.blade.php
+    }
+
+    //     public function createPost()
+    // {
+    //     return "I am post"; // this is just to check page is working 
+    // }
+
+//     // accept the Request object
+    // this gives us access to the submitted data
+    public function createPost(Request $request)
+    {
+    // get all of the submitted data
+    $data = $request->all();
+
+    // create a new owner, passing in the submitted data
+    $owner = Owner::create($data);
+
+    // redirect the browser to the new owner
+    return redirect("/owners/{$owner->id}");
     }
 
 }
