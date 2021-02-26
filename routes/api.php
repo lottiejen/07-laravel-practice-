1cf15d5d965ca966a5821680bb46de8c37fbae9b
Lottie <lottiej17@gmail.com>
2021-02-26 10:13:26 +0000

    api routes updated with animals

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\OwnerController;
+use App\Http\Controllers\API\AnimalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,28 @@ use App\Http\Controllers\API\OwnerController;
 //     return $request->user();
 // });
 
-Route::get("/owners", [OwnerController::class, "index"]); // using this syntax to point to the controller 
-Route::get('/owners/{owner}', [OwnerController::class, 'show']);  // shows owners by specfic ID 
+// owner controller routes
+// the prefix owners means that all of the routes inside that group will start with /owners. 
+Route::group(["prefix" => "owners"], function() {
+
+
+    Route::get('', [OwnerController::class, "index"]); // using this syntax to point to the controller 
+    Route::get('{owner}', [OwnerController::class, "show"]);  // route that shows owners by specfic ID 
+    Route::delete('{owner}', [OwnerController::class, "destroy"]); // route that deletes a user 
+    Route::post('', [OwnerController::class, "store"]);
+
+    
+
+    // animal controller route
+    //   /owners/{owner}/animals e.g. /owners/3/animals   3=owner id
+    Route::get('{owner}/animals', [AnimalController::class, 'index']);
+    //  /owners/{owner}/animals/{owner}
+    Route::get('{owner}/animals/{animal}', [AnimalController::class, 'show']);
+    Route::post('', [AnimalController::class, 'store']);
+    Route::put('{owner}/animals/{animal}', [AnimalController::class, 'update']); // 
+    Route::post('{owner}/animals', [AnimalController::class, 'store']);
+    
+});
+
+
+
